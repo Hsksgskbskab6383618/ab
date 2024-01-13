a25e84a8d6eb0948ccdc90bb5829435bb738102a
Hsksgskbskab6383618 <156532796+Hsksgskbskab6383618@users.noreply.github.com>
2024-01-13 23:06:03 +0600

    Update aa.py

--- aa.py
+++ aa.py
@@ -8,16 +8,16 @@ import threading
 
 
 async def st(u,c):
-    u.reply_text("Hello")
+    u.message.reply_text("Hello")
   
 
 async def ur(u,c):
   
     a = r.get('https://ulvis.net/api.php?url='+u.message.text)
     if "Error" in a.text:
-      u.reply_text("Enter Valid Url")
+      u.message.reply_text("Enter Valid Url")
     else:
-      u.reply_text(a.text)
+      u.message.reply_text(a.text)
 
 
 
