7910984561c02d4f09d308f33fca66bf3b6b9823
Hsksgskbskab6383618 <156532796+Hsksgskbskab6383618@users.noreply.github.com>
2024-01-13 22:52:54 +0600

    Update aa.py

--- aa.py
+++ aa.py
@@ -8,37 +8,41 @@ def aaa():
   pass
 
 
-async def st(u,c):
-  u.reply_text("Hello")
+def abx():
+  async def st(u,c):
+    u.reply_text("Hello")
   
 
-async def ur(u,c):
+  async def ur(u,c):
   
-  a = r.get('https://ulvis.net/api.php?url='+u.message.text)
-  if "Error" in a.text:
-    u.reply_text("Enter Valid Url")
-  else:
-    u.reply_text(a.text)
+    a = r.get('https://ulvis.net/api.php?url='+u.message.text)
+    if "Error" in a.text:
+      u.reply_text("Enter Valid Url")
+    else:
+      u.reply_text(a.text)
 
 
 
-app = ApplicationBuilder().token("6361126109:AAFWJkbNucxUejdyxP3pt8Gn2hEncCcLjHI").build()
+  app = ApplicationBuilder().token("6361126109:AAFWJkbNucxUejdyxP3pt8Gn2hEncCcLjHI").build()
 
-app.add_handler(CommandHandler("start", st))
-app.add_handler(MessageHandler(filters.TEXT,ur))
-
-
-
-ap = Flask(__name__)
-
-@ap.route('/')
-def hm():
-  return 'hi'
+  app.add_handler(CommandHandler("start", st))
+  app.add_handler(MessageHandler(filters.TEXT,ur))
+  app.run_polling()
 
 
+def aby():
 
-x = threading.Thread(target=app.run_polling)
-y = threading.Thread(target=ap.run)
+
+  ap = Flask(__name__)
+
+  @ap.route('/')
+  def hm():
+    return 'hi'
+
+
+
+x = threading.Thread(target=apx)
+y = threading.Thread(target=aby)
 
 
 x.start()
