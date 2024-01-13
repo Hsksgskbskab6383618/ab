3b54fceff6fbbb02028f8bbaf3e31bfadb60395a
Hsksgskbskab6383618 <156532796+Hsksgskbskab6383618@users.noreply.github.com>
2024-01-13 22:32:21 +0600

    Update aa.py

--- aa.py
+++ aa.py
@@ -1,7 +1,11 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes,MessageHandler, Filters
 import requests as r
+from flask import Flask
+import threading
 
+def aaa():
+  pass
 
 
 async def st(u,c):
@@ -20,6 +24,25 @@ async def ur(u,c):
 
 app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
 
-app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("start", st))
+app.add_handler(MessageHandler(Filters.text,ur))
 
-app.run_polling()
+
+
+ap = Flask(__name__)
+
+@ap.route('/')
+def hm():
+  return 'hi'
+
+
+
+x = threading.Thread(target=app.run_polling)
+y = threading.Thread(target=ap.run)
+
+
+x.start()
+y.start()
+
+x.join()
+y.join()
